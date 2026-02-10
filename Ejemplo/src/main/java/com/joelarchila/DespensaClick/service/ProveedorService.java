73ff74a65748ac9d8f6b7dfcae027f97d530b736
joelArchila-2022283 <jarchila-2022283@kinal.edu.gt>
2026-02-10 10:23:47 -0600

    feat: Se agrego la estructura de la clase, anotacion y los métodos

--- Ejemplo/src/main/java/com/joelarchila/DespensaClick/service/ProveedorService.java
+++ Ejemplo/src/main/java/com/joelarchila/DespensaClick/service/ProveedorService.java
@@ -1,4 +1,15 @@
 package com.joelarchila.DespensaClick.service;
 
+import com.joelarchila.DespensaClick.Entity.Proveedor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface ProveedorService {
+    List<Proveedor> getAllProveedores();
+    Proveedor getProveedorById(Integer id);
+    Proveedor saveProveedor (Proveedor proveedor) throws RuntimeException;
+    Proveedor updateProveedor(Integer id, Proveedor proveedor);
+    void deleteProveedor(Integer id);
 }

8a160cf8659a7847167258e0c07054a6d244d600
joelArchila-2022283 <jarchila-2022283@kinal.edu.gt>
2026-02-10 10:14:13 -0600

    feat: Se agrego la estructura de la interfaz, importaciones y anotaciones

--- Ejemplo/src/main/java/com/joelarchila/DespensaClick/repository/ProveedorRepository.java
+++ Ejemplo/src/main/java/com/joelarchila/DespensaClick/repository/ProveedorRepository.java
@@ -1,4 +1,10 @@
 package com.joelarchila.DespensaClick.repository;
 
-public interface ProveedorRepository {
+import com.joelarchila.DespensaClick.Entity.Proveedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface ProveedorRepository extends JpaRepository<Proveedor, Integer> {
 }

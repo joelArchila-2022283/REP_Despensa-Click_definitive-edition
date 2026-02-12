fdee006ac5bd89c6ff13dc2592de0cb2788e4e94
joelArchila-2022283 <jarchila-2022283@kinal.edu.gt>
2026-02-11 23:52:40 -0600

    feat: Se creo en la interfaz el Repository para Producto

--- Ejemplo/src/main/java/com/joelarchila/DespensaClick/repository/ProductoRepository.java
+++ Ejemplo/src/main/java/com/joelarchila/DespensaClick/repository/ProductoRepository.java
@@ -1,4 +1,10 @@
 package com.joelarchila.DespensaClick.repository;
 
-public interface ProductoRepository {
+import com.joelarchila.DespensaClick.Entity.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
 }

0d17ad67e9ea9c2cad6dbb4b8153c46a9f2df6a9
joelArchila-2022283 <jarchila-2022283@kinal.edu.gt>
2026-02-12 17:27:42 -0600

    feat: Se implemento la logica en la clase ProductoServiceImplements

--- Ejemplo/src/main/java/com/joelarchila/DespensaClick/service/ProductoServiceImplements.java
+++ Ejemplo/src/main/java/com/joelarchila/DespensaClick/service/ProductoServiceImplements.java
@@ -1,4 +1,53 @@
 package com.joelarchila.DespensaClick.service;
 
-public class ProductoServiceImplements {
+import com.joelarchila.DespensaClick.Entity.Producto;
+import com.joelarchila.DespensaClick.repository.ProductoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductoServiceImplements implements ProductoService{
+
+    private final ProductoRepository productoRepository;
+
+    public ProductoServiceImplements(ProductoRepository productoRepository) {
+        this.productoRepository = productoRepository;
+    }
+
+    @Override
+    public List<Producto> getAllProductos() {
+        return productoRepository.findAll();
+    }
+
+    @Override
+    public Producto getProductoById(Integer id) {
+        return productoRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Producto saveProducto(Producto producto) throws RuntimeException {
+        return productoRepository.save(producto);
+    }
+
+    @Override
+    public Producto updateProducto(Integer id, Producto producto) {
+        Producto productoExistente = productoRepository.findById(id).orElse(null);
+
+        if(productoExistente != null){
+            productoExistente.setNombre_producto(producto.getNombre_producto());
+            productoExistente.setCategoria_producto(producto.getCategoria_producto());
+            productoExistente.setPrecio_compra(producto.getPrecio_compra());
+            productoExistente.setPrecio_venta(producto.getPrecio_venta());
+            productoExistente.setId_proveedor(producto.getId_proveedor());
+
+            return productoRepository.save(productoExistente);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteProducto(Integer id) {
+        productoRepository.deleteById(id);
+    }
 }

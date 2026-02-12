313714d29e2445a969d1bf74c551facfe88990dc
joelArchila-2022283 <jarchila-2022283@kinal.edu.gt>
2026-02-12 16:57:35 -0600

    feat: Se agrego la estructura de la interfaz, anotaciones y los metodos del CRUD

--- Ejemplo/src/main/java/com/joelarchila/DespensaClick/service/ProductoService.java
+++ Ejemplo/src/main/java/com/joelarchila/DespensaClick/service/ProductoService.java
@@ -1,4 +1,16 @@
 package com.joelarchila.DespensaClick.service;
 
+
+import com.joelarchila.DespensaClick.Entity.Producto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface ProductoService {
+    List<Producto> getAllProductos();
+    Producto getProductoById(Integer id);
+    Producto saveProducto (Producto producto) throws RuntimeException;
+    Producto updateProducto(Integer id, Producto producto);
+    void deleteProducto(Integer id);
 }

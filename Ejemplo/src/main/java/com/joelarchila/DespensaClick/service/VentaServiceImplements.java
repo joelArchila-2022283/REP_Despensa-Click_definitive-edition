e575f0c06195c55bb66381aeccc4f56f081e47d3
joelArchila-2022283 <jarchila-2022283@kinal.edu.gt>
2026-02-12 23:23:07 -0600

    feat: implementa la lógica de negocio en VentaService

---
+++ Ejemplo/src/main/java/com/joelarchila/DespensaClick/service/VentaServiceImplements.java
@@ -0,0 +1,53 @@
+package com.joelarchila.DespensaClick.service;
+
+import com.joelarchila.DespensaClick.Entity.Venta;
+import com.joelarchila.DespensaClick.repository.VentaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VentaServiceImplements implements VentaService{
+
+    private final VentaRepository ventaRepository;
+
+    public VentaServiceImplements(VentaRepository ventaRepository) {
+        this.ventaRepository = ventaRepository;
+    }
+
+    @Override
+    public List<Venta> getAllVentas() {
+        return ventaRepository.findAll();;
+    }
+
+    @Override
+    public Venta getVentaById(Integer id) {
+        return ventaRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Venta saveVenta(Venta venta) throws RuntimeException {
+        return ventaRepository.save(venta);
+    }
+
+    @Override
+    public Venta updateVenta(Integer id, Venta venta) {
+        Venta ventaExistente = ventaRepository.findById(id).orElse(null);
+
+        if(ventaExistente != null){
+            ventaExistente.setFecha_venta(venta.getFecha_venta());
+            ventaExistente.setCantidad(venta.getCantidad());
+            ventaExistente.setTotal(venta.getTotal());
+            ventaExistente.setId_empleado(venta.getId_empleado());
+            ventaExistente.setId_producto(venta.getId_producto());
+
+            return ventaRepository.save(ventaExistente);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteVenta(Integer id) {
+        ventaRepository.deleteById(id);
+    }
+}

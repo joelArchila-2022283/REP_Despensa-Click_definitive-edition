55d5bc01ae44aa0940c3f1311f1e5d93d3a6f360
joelArchila-2022283 <jarchila-2022283@kinal.edu.gt>
2026-02-12 23:04:26 -0600

    feat: crea repositorio JPA para la entidad Venta

---
+++ Ejemplo/src/main/java/com/joelarchila/DespensaClick/repository/VentaRepository.java
@@ -0,0 +1,9 @@
+package com.joelarchila.DespensaClick.repository;
+
+import com.joelarchila.DespensaClick.Entity.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VentaRepository extends JpaRepository<Venta, Integer> {
+}

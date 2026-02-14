9f7e3711c7e7fad5c0875ddee327be4dc832cac3
joelArchila-2022283 <jarchila-2022283@kinal.edu.gt>
2026-02-13 20:31:14 -0600

    feat: validacion campos nulos, ID y dominios de correo permitidos en POST, PUT y DELETE

--- Ejemplo/src/main/java/com/joelarchila/DespensaClick/controller/ProductoController.java
+++ Ejemplo/src/main/java/com/joelarchila/DespensaClick/controller/ProductoController.java
@@ -29,6 +29,17 @@ public class ProductoController {
     //Crear
     @PostMapping
     public ResponseEntity<Object> createProducto(@Valid @RequestBody Producto producto){
+
+        //  Validar Nulos y Vacíos
+        if (producto.getNombre_producto() == null || producto.getCategoria_producto().trim().isEmpty()) {
+            return new ResponseEntity<>("Error: El nombre del producto es obligatorio.", HttpStatus.BAD_REQUEST);
+        }
+
+        // Validar que el precio compra y venta sea mayor a 0
+        if (producto.getPrecio_compra() <= 0 & producto.getPrecio_venta() <= 0) {
+            return new ResponseEntity<>("Error: El precio de compra y de venta debe ser mayor a cero.", HttpStatus.BAD_REQUEST);
+        }
+
         try{
             Producto createdProducto = productoService.saveProducto(producto);
             return new ResponseEntity<>(createdProducto, HttpStatus.CREATED);
@@ -39,7 +50,22 @@ public class ProductoController {
 
     //Actualizar
     @PutMapping("/{id}")
-    public ResponseEntity<Producto> updateProducto(@PathVariable Integer id, @RequestBody Producto producto){
+    public ResponseEntity<Object> updateProducto(@PathVariable Integer id, @RequestBody Producto producto){
+
+        // 1. Validar ID
+        if (id <= 0) {
+            return new ResponseEntity<>("Error: El ID debe ser un número positivo.", HttpStatus.BAD_REQUEST);
+        }
+
+        // 2. Validaciones de lógica de negocio
+        if (producto.getPrecio_compra() <= 0 && producto.getPrecio_venta() <= 0) {
+            return new ResponseEntity<>("Error: El nuevo precio debe ser mayor a cero.", HttpStatus.BAD_REQUEST);
+        }
+
+        if (producto.getNombre_producto() == null || producto.getCategoria_producto().trim().isEmpty()) {
+            return new ResponseEntity<>("Error: El nombre y la categoria no puede quedar vacío.", HttpStatus.BAD_REQUEST);
+        }
+
         Producto actualizado = productoService.updateProducto(id, producto);
 
         if(actualizado != null){
@@ -52,6 +78,11 @@ public class ProductoController {
     //Eliminar
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteProducto(@PathVariable Integer id){
+
+        if (id <= 0) {
+            return new ResponseEntity<>("Error: ID inválido.", HttpStatus.BAD_REQUEST);
+        }
+
         Producto producto = productoService.getProductoById(id);
 
         if(producto != null){
